fca55d070bf4c5699fcef2347f43be94e764e085
jasunk <jskraakenes@gmail.com>
2022-11-11 12:36:44 +0100

    nytt system :+1:

--- doom/code.js
+++ doom/code.js
@@ -1,16 +1,19 @@
 /* Definerer gameobjects */
-let alive = [true,true,false, true]
-let left = [0,500,-900, -600]
-let ref = [".enemy1", ".enemy2", ".house", ".enemy3"]
-let scale = [0.2,0.2,1, 0.1]
-let speed = [0.01, 0.01, 0, 0.01]
+
+let alive = [true,false]
+let left = [0,-900]
+let ref = [".enemy1",".house"]
+let scale = [0.2,1]
+let speed = [0.01, 0]
 let score = 0
 
-let newAlive = [true,true,true]
-let newLeft = [200, 1000, -400]
-let newRef = [".enemy4",".enemy5",".enemy6"]
-let newScale = [0.2,0.1,0.3]
-let newSpeed = [0.01,0.015,0.075]
+let newAlive = [false,false,false,false,false,false,false,false,false,false,false,false,false,false]
+let newLeft = [500, -600, 200, 900, 300,1500, 300, 1100, 500, -430,200, -300, 1200, 1000]
+let newRef = [".enemy2",".enemy3", ".enemy4",".enemy5",".enemy6",".enemy7",".enemy8",".enemy9",".enemy10",".enemy11",".enemy12",".enemy13",".enemy14"]
+let newScale = [0.2,0.2,0.2,0.1,0.3,0.2,0.2,0.2,0.1,0.3,0.2,0.2,0.2,0.1]
+let newSpeed = [0.01,0.01,0.01,0.015,0.01,0.01,0.01,0.01,0.015,0.01,0.01,0.01,0.01,0.015]
+
+
 
 let mouseAccel=1
 const vw = (Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0))/2
@@ -21,12 +24,21 @@ for (let i = 0; i < ref.length; i++) {document.querySelector(ref[i]).style.margi
 document.addEventListener('keydown', function(event) {
 
     if(event.keyCode == 32) {
-        shoot(); 
+        shoot(); spawn()
         for (let i = 0; i < ref.length; i++) {
             if (left[i]<100 && left[i]>-100){
                 score++
+                
                 document.querySelector("#score").innerHTML = score
                 document.querySelector(ref[i]).classList.add("die"); kill(i);
+            } 
+        }
+        for (let i = 0; i < newRef.length; i++) {
+            if (newLeft[i]<100 && newLeft[i]>-100){
+                score++
+                
+                document.querySelector("#score").innerHTML = score
+                document.querySelector(newRef[i]).classList.add("die"); kill(i);
             } 
         }
 
@@ -35,7 +47,9 @@ document.addEventListener('keydown', function(event) {
         mouseAccel+=7.5
         for (let i = 0; i < ref.length; i++) {
             left[i]+= 35+mouseAccel
-            
+        }
+        for (let i = 0; i < newRef.length; i++) {
+            newLeft[i]+= 35+mouseAccel
         }
         
     }
@@ -45,11 +59,20 @@ document.addEventListener('keydown', function(event) {
         for (let i = 0; i < ref.length; i++) {
             left[i]-= 35+mouseAccel
         }
+        for (let i = 0; i < newRef.length; i++) {
+            newLeft[i]-= 35+mouseAccel
+        }
     }
     
     for (let i = 0; i < ref.length; i++) {
         document.querySelector(ref[i]).style.marginLeft = left[i].toString()+"px"
     }
+    for (let i = 0; i < newRef.length; i++) {
+        document.querySelector(newRef[i]).style.marginLeft = newLeft[i].toString()+"px"
+    }
+   
+    
+    
     
 });
 
@@ -60,6 +83,14 @@ document.addEventListener("keyup",function(){
 
 setInterval(grow, 100)
 function grow(){
+    for (let i = 0; i < newScale.length; i++) {
+        if (newAlive[i]==true){
+            if (newAlive[i]){newScale[i]+= newSpeed[i]}
+            
+            document.querySelector(newRef[i]).style.transform = "scale("+newScale[i].toString()+ ","+newScale[i].toString()+")"
+        }        
+    }
+    
     
     
         for (let i = 0; i < scale.length; i++) {
@@ -73,6 +104,11 @@ function grow(){
 function kill(i){
     
     alive[i]=false
+
+    for (let i = 0; i < ref.length; i++) {
+        if (ref[i].classList=="die")
+        {ref[i].classList.add("dead")}
+    }
     
     /* delete alive[i];
     delete left[i];
@@ -91,7 +127,7 @@ function kill(i){
 }
 
 function shoot(){
-    spawn()
+    
     document.getElementById("gun").classList.add("shoot")
     setTimeout(gunreset, 500)
 }
@@ -99,17 +135,24 @@ function shoot(){
 function gunreset(){
     document.getElementById("gun").classList.remove("shoot")
 }
-
+let nbrSpawned = 0
 function spawn(){
-    const node = document.getElementById("enemyInactive");
+    /* const node = document.getElementById("enemyInactive");
     const clone = node.cloneNode(true);
     document.body.appendChild(clone);
     
     clone.classList.add("enemy")
     clone.classList.add(newRef[0])
-    clone.style.marginLeft = newLeft[0].toString +"px"
-    console.log(ref)
-    let mengde = ref.length+1
+    clone.style.marginLeft = newLeft[0].toString() +"px"
+    console.log(ref) */
+
+    document.querySelector(newRef[nbrSpawned]).classList.remove("disabled")
+    newAlive[nbrSpawned]=true
+    nbrSpawned++
+
+
+    
+   /*  let mengde = ref.length+1
     
     alive.push(newAlive[0])
     ref.push(newRef[0])
@@ -118,7 +161,7 @@ function spawn(){
     scale.push(newScale[0])
 
     console.log("alive:",alive,"ref", ref,"left", left,"speed", speed,"scale", scale)
-
-    newRef.shift();
+ */
+    
      
 }
