291ad088fb6622a793c11237d2ff5c825863411f
jasunk <jskraakenes@gmail.com>
2022-11-26 23:36:00 +0100

    survival?

---
+++ 2dSurvival/code.js
@@ -0,0 +1,153 @@
+let divsHoriz =window.innerWidth/ (window.innerWidth/100*2.5)
+let divnr= divsHoriz*25
+let pTop = 0.5
+let pLeft = 0.5
+let canmove = true
+let canAttack =true
+let level = []
+let speed = 50
+let playerRef = document.getElementById("visual")
+
+
+for (let i = 0; i < divnr; i++) {
+    document.querySelector("#level").appendChild(document.createElement('div'))
+   if (i==1) {
+    document.querySelector("#level").childNodes[1].appendChild(document.createElement('div'))
+    document.querySelector("#level").childNodes[1].childNodes[0].classList.add("character")
+  }
+  if (i>0){
+    document.querySelector("#level").childNodes[i].classList.add("ground")
+    level.push(document.querySelector("#level").childNodes[i])
+  }
+  
+}
+document.body.removeChild(document.body.childNodes[1])
+
+setInterval(update,50)
+function update(){
+  playerRef.style.left = pLeft + "vw"
+    playerRef.style.top = pTop + "vw"
+}
+window.addEventListener("keydown", function (event) {
+    if (event.defaultPrevented) {
+      return; // Do nothing if the event was already processed
+    }
+    
+    
+    switch (event.key) {
+        case "s":
+        movement(40)
+        break;
+      case "w":
+        movement(-divsHoriz)
+        break;
+      case "a":
+        movement(-1)
+        break;
+      case "d":
+        movement(1)
+        break;
+      case " ":
+        attack()
+        break;
+      default:
+        return; // Quit when this doesn't handle the key event.
+    }
+    
+    // Cancel the default action to avoid it being handled twice
+    event.preventDefault();
+  }, true);
+  // the last option dispatches the event to the listener first,
+  // then dispatches event to window
+/* window.addEventListener("click",clickHandler)
+
+function clickHandler(e) {
+    alert(mouseB)
+    var target = (e.target) ? e.target : e.srcElement;
+    target.classList.add("wall")
+} */
+var mouseB;
+window.addEventListener('click', (event) => {
+    console.log(event.button)
+    mouseB=0
+    var target = (event.target) ? event.target : event.srcElement;
+    if (target.classList != "unclickable"){target.classList.add("wall"); target.classList.remove("ground")}
+
+    
+})
+
+window.addEventListener('contextmenu', (event) => {
+    console.log(event.button)
+    mouseB=1
+    var target = (event.target) ? event.target : event.srcElement;
+    if (target.classList != "unclickable"){target.classList.remove("wall"); target.classList.add("ground")}
+})
+
+let divs = document.querySelector("#level").children
+
+function movement(levelCheck){
+  
+  for (let i = 0; i < divs.length; i++) {        
+    if (divs[i].firstChild && divs[i].firstChild.classList=="character" ){   
+        if (canmove && level[(i+levelCheck)].classList == "ground" ){
+            canmove=false
+            setTimeout(resetMove,speed)
+            var original = document.querySelector(".character");
+            var clone = original.cloneNode(true);
+            (divs[(i+levelCheck)]).appendChild(clone);
+            divs[i].removeChild(divs[i].firstChild)  
+            if (levelCheck==1){
+              pLeft+=2.5
+            }else if (levelCheck==-1){
+              pLeft-=2.5
+            }else if (levelCheck==divsHoriz){
+              pTop+=2.5
+            }else {
+              pTop-=2.5
+            }
+        }  
+    }
+  }
+ 
+}
+
+function resetMove(){
+  canmove=true
+}
+let whatRemove = []
+let attackPos= [1,-1,divsHoriz,-divsHoriz]
+function attack(){
+  
+  for (let i = 0; i < divs.length; i++) {     
+    
+    if (canAttack && divs[i].firstChild && divs[i].firstChild.classList=="character"){   
+      for (let a = 0; a < attackPos.length; a++) {
+        
+        
+      if (level[(i+attackPos[a])].classList == "ground" ){
+            
+           
+            level[(i+attackPos[a])].appendChild(document.createElement('div'))
+            level[(i+attackPos[a])].childNodes[0].classList.add("attack")
+            whatRemove.push(level[(i+attackPos[a])])
+            setTimeout(remove,500)
+            console.log(whatRemove)
+            
+
+            
+        }  
+      }
+    }
+    
+  }canAttack=false
+}
+
+function remove(){
+  canAttack=true
+  for (let i = 0; i < whatRemove.length; i++) {
+    whatRemove[i].removeChild(whatRemove[i].firstChild)
+    
+  }
+  
+  whatRemove.length=0
+}
